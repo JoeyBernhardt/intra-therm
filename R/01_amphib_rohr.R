71fabc507f203394f05877ccde39be4ccbbd7355
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2018-05-10 15:07:51 -0700

    adding figures

--- R/01_amphib_rohr.R
+++ R/01_amphib_rohr.R
@@ -2,6 +2,8 @@
 
 library(tidyverse)
 library(janitor)
+library(viridis)
+library(cowplot)
 
 
 amphib <- read_csv("data-raw/amphib-rohr.csv") %>% 
@@ -13,13 +15,23 @@ am2 <- amphib %>%
 	unite(col = record_species, sep = "_", remove = FALSE, record, genus_species) 
 
 mult_pop <- am2 %>% 
-	distinct(genus_species, latitude) %>% 
+	distinct(genus_species, latitude, longitude) %>% 
 	group_by(genus_species) %>% 
 	tally() %>%
 	filter(n > 1) %>% 
 	select(genus_species)
 
+
+
 amphib_multi_pop <- am2 %>% 
 	filter(genus_species %in% mult_pop$genus_species) 
 
 write_csv(amphib_multi_pop, "data-processed/rohr_amphib_multi_pop.csv")
+
+
+amphib_multi_pop %>% 
+	ggplot(aes(x = abs_lat, y = raw_ctm1)) + geom_point() +
+	geom_smooth(method = "lm", color = "black") +
+	theme(legend.position = "none") + facet_wrap(~ genus_species, scales = "free") +
+	ylab("CTmax") + xlab("Absolute latitude")
+ggsave("figures/amphib_multipop.pdf", width = 14, height = 14)
