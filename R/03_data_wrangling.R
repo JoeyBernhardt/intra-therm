e3aa3494ddf2b235034b7c29b03af17e16ef6a76
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2018-06-16 12:03:38 -0400

    adding data subset

---
+++ R/03_data_wrangling.R
@@ -0,0 +1,34 @@
+
+
+### data wrangling
+library(tidyverse)
+
+
+AB <- read_csv("data-raw/Globtherm2_within_species_AB.csv")
+FL <- read_csv("data-raw/Globtherm2_within_species_FL.csv")
+FV <- read_csv("data-raw/Globtherm2_within_species_FV.csv")
+SO <- read_csv("data-raw/Globtherm2_within_species_SO.csv")
+
+
+FL2 <- FL %>% 
+	select(1:9) %>% 
+	select(-sample_size)
+
+AB2 <- FL %>% 
+	select(1:9) %>% 
+	select(-sample_size)
+
+SO2 <- SO %>% 
+	select(1:9)%>% 
+	select(-sample_size)
+FV2 <- FV %>% 
+	select(1:9)%>% 
+	select(-sample_size)
+
+all <- bind_rows(AB2, FL2, SO2, FV2)
+
+
+str(AB)
+
+write_csv(all, "data-processed/intra-therm-mini.csv")
+
