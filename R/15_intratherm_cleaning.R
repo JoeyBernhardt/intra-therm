343f1deedd0596f49b7dbe89fc90843b5a9a9e1c
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2020-05-01 10:29:55 -0400

    adding new data from Nikki

--- R/15_intratherm_cleaning.R
+++ R/15_intratherm_cleaning.R
@@ -6,4 +6,16 @@
 ## this is the latest datafile sent by Nikki (April 30 2020)
 
 intratherm <- read_csv("data-raw/intratherm-merged-nikkis-traits-clean.csv") %>%
-	mutate(population_id = paste(genus_species, latitude, sep = "_"))
+	mutate(population_id = paste(genus_species, latitude, sep = "_")) %>% 
+	arrange(genus_species)
+
+
+names(intratherm)[grepl("stage", names(intratherm))] ## find the list of columns that have 'general'
+
+unique(intratherm$dispersal_distance_category)
+
+
+
+intratherm %>% 
+	filter(is.na(longitude)) %>% View
+	mutate(dispersal_distance_category = ifelse(dispersal_distance_category == "10-Jan", "1-10", dispersal_distance_category))
