fe6b49f11cf5adec2125013f7ec25e545416093a
nicole-a-moore <nicole.moore@mail.mcgill.ca>
2020-05-20 10:51:31 -0400

    updating freshwater temperature data to include populations that have the same location but are different species

--- R/19_get_freshwater_temp_data_nikki.R
+++ R/19_get_freshwater_temp_data_nikki.R
@@ -91,8 +91,41 @@ converted$date <- as.vector(date_name)
 converted$date_number <- as.vector(date_num)
 
 converted <- converted %>%
-	select(date, date_number, everything())
+	dplyr::select(date, date_number, everything())
+
+temperature_data <- converted
+
+## add rows for populations with the same lat, long and elevation but different species 
+## will have same temperature data 
+populations <- freshwater[!duplicated(freshwater[,c("genus_species", "latitude", "longitude", "elevation_of_collection")]),]
+
+i <- 1
+while (i < length(unique_pairs$population_id) + 1) {
+	z <- 1
+	while (z < length(populations$population_id) + 1) {
+		same_pop <- (unique_pairs$latitude[i] == populations$latitude[z] & 
+					 	unique_pairs$longitude[i] == populations$longitude[z] & 
+					 	unique_pairs$genus_species[i] == populations$genus_species[z])
+		
+		same_loc <- (unique_pairs$latitude[i] == populations$latitude[z] & 
+					 	unique_pairs$longitude[i] == populations$longitude[z])
+		
+		if (!same_pop & same_loc) {
+			temperature_data$temps <- temperature_data[,i+2]
+			pop_id <- paste(populations$population_id[z], populations$longitude[z], sep = "_")
+			colnames(temperature_data)[length(temperature_data)]<- pop_id
+			same_pop = FALSE
+			same_loc = FALSE
+			z = z+1
+		}
+		else {
+			z = z+1
+		}
+	}
+	i = i+1
+}
+
 
 
 ## write temperature file to processed data 
-write.csv(converted, "./data-processed/intratherm-freshwater-temp-data.csv")
+write.csv(temperature_data, "./data-processed/intratherm-freshwater-temp-data.csv")
